b3d2172b0763c88cc678091d2085aeabf7dafe54
Dan Nixon <dan@dan-nixon.com>
2023-05-11 17:07:26 +0100

    Include Git revision in version output

---
+++ common/src/version.rs
@@ -0,0 +1,10 @@
+#[macro_export]
+macro_rules! version {
+    () => {
+        format!(
+            "v{} git={}",
+            clap::crate_version!(),
+            git_version::git_version!()
+        )
+    };
+}
