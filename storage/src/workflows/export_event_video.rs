dcffdcec77f4d03af185aba6ca2ecfeb931d9a5c
Dan Nixon <dan@dan-nixon.com>
2023-05-29 15:44:53 +0100

    Add generate filename operation to storage workflows

--- storage/src/workflows/export_event_video.rs
+++ storage/src/workflows/export_event_video.rs
@@ -1,8 +1,17 @@
 use crate::{Provider, StorageError, StorageProvider, StorageResult};
 use bytes::{BufMut, Bytes};
 use satori_common::{CameraSegments, Event};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use tracing::info;
+
+pub fn generate_video_filename(
+    event: &Event,
+    camera_name: Option<String>,
+) -> StorageResult<PathBuf> {
+    let timestamp = event.metadata.timestamp.to_rfc3339();
+    let camera = get_camera_from_event_by_name(event, camera_name)?;
+    Ok(PathBuf::from(format!("{timestamp}_{0}.mp4", camera.name)))
+}
 
 pub async fn export_event_video(
     storage: Provider,
@@ -58,6 +67,66 @@ mod test {
     use chrono::Utc;
     use satori_common::{Event, EventMetadata};
     use std::path::PathBuf;
+
+    #[test]
+    fn test_generate_video_filename_default_camera() {
+        let event = Event {
+            metadata: EventMetadata {
+                id: "test".into(),
+                timestamp: chrono::NaiveDate::from_ymd_opt(2022, 12, 30)
+                    .unwrap()
+                    .and_hms_opt(18, 8, 0)
+                    .unwrap()
+                    .and_local_timezone(chrono::FixedOffset::east_opt(0).unwrap())
+                    .unwrap(),
+            },
+            start: Utc::now().into(),
+            end: Utc::now().into(),
+            reasons: Default::default(),
+            cameras: vec![CameraSegments {
+                name: "camera1".into(),
+                segment_list: vec![PathBuf::from("1_2.ts"), PathBuf::from("1_3.ts")],
+            }],
+        };
+
+        assert_eq!(
+            generate_video_filename(&event, None).unwrap(),
+            PathBuf::from("2022-12-30T18:08:00+00:00_camera1.mp4")
+        );
+    }
+
+    #[test]
+    fn test_generate_video_filename_specified_camera() {
+        let event = Event {
+            metadata: EventMetadata {
+                id: "test".into(),
+                timestamp: chrono::NaiveDate::from_ymd_opt(2022, 12, 30)
+                    .unwrap()
+                    .and_hms_opt(18, 8, 0)
+                    .unwrap()
+                    .and_local_timezone(chrono::FixedOffset::east_opt(0).unwrap())
+                    .unwrap(),
+            },
+            start: Utc::now().into(),
+            end: Utc::now().into(),
+            reasons: Default::default(),
+            cameras: vec![
+                CameraSegments {
+                    name: "camera1".into(),
+                    segment_list: vec![PathBuf::from("1_2.ts"), PathBuf::from("1_3.ts")],
+                },
+                CameraSegments {
+                    name: "camera2".into(),
+                    segment_list: vec![PathBuf::from("2_2.ts"), PathBuf::from("2_3.ts")],
+                },
+            ],
+        };
+
+        assert_eq!(
+            generate_video_filename(&event, Some("camera2".into())).unwrap(),
+            PathBuf::from("2022-12-30T18:08:00+00:00_camera2.mp4")
+        );
+    }
 
     #[tokio::test]
     async fn test_export_event_video() {
