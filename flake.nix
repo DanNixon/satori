21c5113b1bc9fd0874ecad35dbc07e15a03425e9
Dan Nixon <dan@dan-nixon.com>
2023-09-24 23:27:05 +0000

    Tidy Nix flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,16 @@
 {
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    naersk.url = "github:nix-community/naersk";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils, fenix, naersk }:
