39839abf52a2159492ea48c1c6471df5ee15e8fb
Dan Nixon <dan@dan-nixon.com>
2024-01-28 15:07:43 +0000

    wip

--- web/src/config.rs
+++ web/src/config.rs
@@ -5,12 +5,11 @@ use url::Url;
 #[derive(Debug, Deserialize)]
 pub(crate) struct Config {
     cameras: HashMap<String, CameraDetails>,
-    // TODO: MQTT config
 }
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct CameraDetails {
-    hls_stream: Url,
-    frame_image_url: Url,
-    agent_metrics: Url,
+    thumbnail_url: Url,
+    mjpeg_url: Url,
+    hls_url: Url,
 }
