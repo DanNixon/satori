f77eb4c5fa5f7600775d7276c3791349795f0e79
Dan Nixon <dan@dan-nixon.com>
2024-03-29 15:17:07 +0000

    wip

--- web/default.nix
+++ web/default.nix
@@ -21,29 +21,28 @@
     };
   };
 
-  satori-web-container-image =
-    pkgs.dockerTools.buildImage {
-      name = "satori-web";
-      tag = "latest";
-      created = "now";
+  satori-web-container-image = pkgs.dockerTools.buildImage {
+    name = "satori-web";
+    tag = "latest";
+    created = "now";
 
-      copyToRoot = pkgs.buildEnv {
-        name = "image-root";
-        paths = with pkgs; [bashInteractive coreutils];
-        pathsToLink = ["/bin"];
-      };
-
-      config = {
-        Entrypoint = ["${pkgs.tini}/bin/tini" "--" "${satori-web}/bin/satori-web"];
-        ExposedPorts = {
-          "8000/tcp" = {};
-          "9090/tcp" = {};
-        };
-        Env = [
-          "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-          "HTTP_SERVER_ADDRESS=0.0.0.0:8000"
-          "OBSERVABILITY_ADDRESS=0.0.0.0:9090"
-        ];
-      };
+    copyToRoot = pkgs.buildEnv {
+      name = "image-root";
+      paths = with pkgs; [bashInteractive coreutils];
+      pathsToLink = ["/bin"];
     };
-  }
+
+    config = {
+      Entrypoint = ["${pkgs.tini}/bin/tini" "--" "${satori-web}/bin/satori-web"];
+      ExposedPorts = {
+        "8000/tcp" = {};
+        "9090/tcp" = {};
+      };
+      Env = [
+        "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+        "HTTP_SERVER_ADDRESS=0.0.0.0:8000"
+        "OBSERVABILITY_ADDRESS=0.0.0.0:9090"
+      ];
+    };
+  };
+}
