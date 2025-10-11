cdb2cf191bcaa9d3081c5850438d6c71bf20b127
Dan Nixon <dan@dan-nixon.com>
2025-10-11 15:43:37 +0100

    Remove use of byte-unit

--- satori-agent/src/utils.rs
+++ satori-agent/src/utils.rs
@@ -1,21 +1,20 @@
-use byte_unit::Byte;
 use std::{fs, path::Path};
 
-pub(crate) fn get_size<P>(path: P) -> std::io::Result<Byte>
+pub(crate) fn get_size<P>(path: P) -> std::io::Result<u64>
 where
     P: AsRef<Path>,
 {
-    let mut result: u128 = 0;
+    let mut result: u64 = 0;
 
     for entry in fs::read_dir(&path)? {
         let path = entry?.path();
 
         if path.is_file() {
-            result += path.metadata()?.len() as u128;
+            result += path.metadata()?.len();
         } else {
-            result += get_size(path)?.get_bytes();
+            result += get_size(path)?;
         }
     }
 
-    Ok(Byte::from_bytes(result))
+    Ok(result)
 }
